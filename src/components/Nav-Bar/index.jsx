92518f6608ec91b98975f27b08335185e598a6e3
rafael-leivas <rafaelblsteam@gmail.com>
2024-02-27 19:55:41 -0300

    Criando a NavBar da requisição de teste.

---
+++ src/components/Nav-Bar/index.jsx
@@ -0,0 +1,15 @@
+import "./styles.css";
+
+const NavBar = () => {
+  return (
+    <div className="container">
+      <div className="itens">
+        <a href="#">Home</a>
+        <a href="#">Humano</a>
+        <a href="#">Alien</a>
+      </div>
+    </div>
+  );
+};
+
+export default NavBar;
---
+++ src/template/Home/Index.js
@@ -0,0 +1,14 @@
+import NavBar from '../../components/Nav-Bar';
+import './Styles.css';
+
+function Home() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <NavBar />
+      </header>
+    </div>
+  );
+}
+
+export default Home;
